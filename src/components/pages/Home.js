d9ffd3f9f39081aeab3c0a44a42f3c0438c30537
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2021-09-02 12:25:29 +0500

    creating home component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Jumbotron, Container } from 'reactstrap';
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <>
+      <Jumbotron fluid>
+        <Container fluid className={'mt-5'}>
+          <h1 className='display-4'>Welcome!</h1>
+          <p className='lead'>Hi, welcome to my website.</p>
+        </Container>
+      </Jumbotron>
+    </>
+  );
 };
 
 export default Home;
