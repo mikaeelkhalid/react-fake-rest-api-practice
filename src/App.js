8937ed1e281bef6ce07297c202749a6fc1b6eac5
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2021-09-02 12:23:35 +0500

    routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import { Container } from 'reactstrap';
+import NavBar from './components/NavBar';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Home from './components/pages/Home';
+import Airlines from './components/pages/Airlines';
+import Passengers from './components/pages/Passengers';
+import Settings from './components/pages/Settings';
+import Logout from './components/pages/Logout';
+
 function App() {
   return (
-    <div className='App'>
-      <h1>hello</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Container>
+          <NavBar />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/airlines' component={Airlines} />
+            <Route path='/passengers' component={Passengers} />
+            <Route path='/settings' component={Settings} />
+            <Route path='/logout' component={Logout} />
+          </Switch>
+        </Container>
+      </BrowserRouter>
+    </>
   );
 }
 
