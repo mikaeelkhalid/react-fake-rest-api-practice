3960dca7cad12c14f9db75b1bbeb39a67f4805f7
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2021-09-02 16:20:38 +0500

    getting airlines data

--- src/components/pages/Airlines.js
+++ src/components/pages/Airlines.js
@@ -1,15 +1,47 @@
-import React from 'react';
-import { Jumbotron, Container } from 'reactstrap';
+import { useEffect } from 'react';
+import { Jumbotron, Container, Row, Col } from 'reactstrap';
+import { useSelector, useDispatch } from 'react-redux';
+import { getAirline } from '../../redux/reducers/airlines';
+import AirlineCard from './../AirlineCard';
 
 const Airlines = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAirline());
+  }, [dispatch]);
+
+  const airlines = useSelector((state) => state.airlines.airline);
+
+  const airlinesData = airlines.length ? airlines.slice(0, 16) : [];
+
+  const cardData = airlines.length ? (
+    airlinesData.map((airline) => (
+      <Col sm='3' className={'mt-3'} key={airline.id}>
+        <AirlineCard airline={airline} />
+      </Col>
+    ))
+  ) : (
+    <div>Loading Passengers Data...</div>
+  );
+
   return (
     <>
       <Jumbotron fluid>
         <Container fluid className={'mt-5'}>
           <h1 className='display-4'>Airlines!</h1>
-          {/* <p className='lead'>...</p> */}
+          <p className='lead'>
+            Getting Airlines using redux/redux saga and axios
+          </p>
+          <p className='lead'>
+            Total Airlines: {airlines.length ? airlines.length : 'Loading..'}
+          </p>
         </Container>
       </Jumbotron>
+
+      <Container fluid className={'mt-5 mb-5'}>
+        <Row>{cardData}</Row>
+      </Container>
     </>
   );
 };
