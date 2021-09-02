68eff4c8e6a79348230abe9f1440328611ce7e43
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2021-09-02 12:28:18 +0500

    creating settings component

--- src/components/pages/Settings.js
+++ src/components/pages/Settings.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Jumbotron, Container } from 'reactstrap';
 
 const Settings = () => {
-  return <div></div>;
+  return (
+    <>
+      <Jumbotron fluid>
+        <Container fluid className={'mt-5'}>
+          <h1 className='display-4'>Settings!</h1>
+          {/* <p className='lead'>...</p> */}
+        </Container>
+      </Jumbotron>
+    </>
+  );
 };
 
 export default Settings;
