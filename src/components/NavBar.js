31d46d7292fb84aa129254d18940b90f03a6d838
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2021-09-02 12:22:43 +0500

    creating navbar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,68 @@
-import React from 'react';
+import { useState } from 'react';
+import { NavLink, withRouter } from 'react-router-dom';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  Container,
+} from 'reactstrap';
 
 const NavBar = () => {
-  return <div></div>;
+  const [collapsed, setCollapsed] = useState(true);
+  const toggleNavbar = () => setCollapsed(!collapsed);
+
+  const myStyle = {
+    color: 'inherit',
+    textDecoration: 'inherit',
+    display: 'block',
+    padding: '0.5rem 31rem',
+    paddingRight: '0',
+    paddingLeft: '0',
+  };
+  return (
+    <>
+      <Container>
+        <Navbar color='faded' light>
+          <NavbarBrand href='/' className='mr-auto'>
+            React Assignment
+          </NavbarBrand>
+          <NavbarToggler onClick={toggleNavbar} className='mr-2' />
+          <Collapse isOpen={!collapsed} navbar>
+            <Nav navbar>
+              <NavItem>
+                <NavLink style={myStyle} to='/'>
+                  Home
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink style={myStyle} to='/airlines'>
+                  Airlines
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink style={myStyle} to='/passengers'>
+                  Passengers
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink style={myStyle} to='/settings'>
+                  Settings
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink style={myStyle} to='/logout'>
+                  Logout
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </Container>
+    </>
+  );
 };
 
-export default NavBar;
+export default withRouter(NavBar);
