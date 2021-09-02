62fe6b00468d62646f08be679b648f1bd41b89cd
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2021-09-02 12:27:43 +0500

    getting passengers data from api

--- src/components/pages/Passengers.js
+++ src/components/pages/Passengers.js
@@ -1,7 +1,64 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Jumbotron, Container, Row, Col } from 'reactstrap';
+
+import PassengerCard from '../PassengerCard';
+import Pagination from '../Pagination';
 
 const Passengers = () => {
-  return <div></div>;
+  const [passengers, setPassengers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [pagination, setPagination] = useState(0);
+
+  const getPassengers = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.instantwebtools.net/v1/passenger?page=${pagination}&size=${
+          pagination + 8
+        }`
+      );
+      const { data } = response;
+      setPassengers(data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getPassengers();
+  }, [pagination]);
+
+  const cardData = isLoading ? (
+    <div>Loading Passengers Data...</div>
+  ) : (
+    passengers.data.map((passenger) => (
+      <Col sm='3' className={'mt-3'}>
+        <PassengerCard key={passenger._id} passenger={passenger} />
+      </Col>
+    ))
+  );
+
+  // console.log(passengers);
+  return (
+    <>
+      <Jumbotron fluid>
+        <Container fluid className={'mt-5'}>
+          <h1 className='display-4'>Passengers!</h1>
+          <p className='lead'>
+            Total Passengers {isLoading ? 0 : `${passengers.totalPassengers}`}
+          </p>
+        </Container>
+      </Jumbotron>
+
+      <Container fluid className={'mt-5'}>
+        <Row>{cardData}</Row>
+      </Container>
+      <Container fluid className={'mt-5 mb-5'}>
+        <Pagination pagination={pagination} setPagination={setPagination} />
+      </Container>
+    </>
+  );
 };
 
 export default Passengers;
